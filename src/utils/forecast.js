de46fe65966386df2b65d8e5ac64250cd86a56be
Alibay Malik-Abbas <alibay.malik@gmail.com>
2019-09-26 03:21:54 +0630

    updating forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const key = 'd9ce6ec78b3fe59018019eb00e820f40'
-    const url = 'https://api.darksky.net/forecast/' + key + '/' + + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/' + key + '/' + + latitude + ',' + longitude + '?units=si'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -11,7 +11,9 @@ const forecast = (latitude, longitude, callback) => {
             console.log(body.error)
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.' +
+                ' With humidity of  ' + body.currently.humidity * 100 + '%')
+
         }
     })
 }
